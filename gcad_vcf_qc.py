aeb5a71d11665045bf1f57c6f64534ad30bfeb40
Taha Iqbal <taha.iqbal@pennmedicine.upenn.edu>
2023-10-09 13:33:37 -0400

    bug fix: AF 0 when no clean samples

--- gcad_vcf_qc.py
+++ gcad_vcf_qc.py
@@ -1094,7 +1094,7 @@ def main():
                 rec.info['AC'] = 2*clean_obs[2] + clean_obs[1]
                 
                 #Append Allele Frequency to INFO field
-                rec.info['AF'] = float((clean_obs[1] + (2 * clean_obs[2]))/ (2 * sum(clean_obs)))
+                rec.info['AF'] = float((clean_obs[1] + (2 * clean_obs[2]))/ (2 * sum(clean_obs)))  if sum(clean_obs) > 0 else 0
 
                 # Append subset VFLAGS to INFO field
                 rec.info[ "VFLAGS_" + subset ] = vf
